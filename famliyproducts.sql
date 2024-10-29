44f442f126d15863cc67932a18159b13dbb60d87
RawanAlha <peoplerere033@gmail.com>
2024-10-29 04:49:57 +0300

    Update famliyproducts.sql
    by Rawan

--- famliyproducts.sql
+++ famliyproducts.sql
@@ -1,1 +1,152 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `famliyproducts`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin@gmail.com', '$2y$10$rmBnmFdpbtw4DatgAogaf.uAJp.EYQs8koJ.I7ThwspQw7SVYo3ce');
+
+
+CREATE TABLE `assigned_orders` (
+  `id` int(11) NOT NULL,
+  `od_id` int(11) NOT NULL,
+  `dv_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+CREATE TABLE `business_type` (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `u_id` int(11) NOT NULL,
+  `total` float DEFAULT 0,
+  `is_applied` tinyint(1) DEFAULT 0,
+  `promo` float DEFAULT 0,
+  `is_add_w` tinyint(1) DEFAULT 0,
+  `wl_amt` float DEFAULT 0,
+  `final_amt` float DEFAULT 0,
+  `ship_fee` tinyint(1) DEFAULT 0,
+  `belonging_city` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `cart_detail` (
+  `id` int(11) NOT NULL,
+  `cart_id` int(11) NOT NULL,
+  `p_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `categories` (`id`, `category`, `status`) VALUES
+(1, 'food', 1);
+
+
+
+CREATE TABLE `city` (
+  `id` int(11) NOT NULL,
+  `s_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `city_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `cnfrm_delivery` (
+  `id` int(11) NOT NULL,
+  `od_id` int(11) NOT NULL,
+  `dv_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ALTER TABLE `cnfrm_delivery`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `cnfrm_delivery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `city`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `cart_detail`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `cart_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+ALTER TABLE `business_type`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `business_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `assigned_orders`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `assigned_orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
