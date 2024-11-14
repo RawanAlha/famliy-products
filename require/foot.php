4cf41955f32166da7cf7bc9c602fa9cc686aa2b4
Saragvkuj <saarra.00@icloud.com>
2024-11-14 04:29:24 +0300

    Update foot.php

---
+++ require/foot.php
@@ -0,0 +1,135 @@
+<section class="defaultPadding mt4 footer">
+    <div class="footer-row1">
+        <div class="container">
+            <div class="row flex">
+                <ul class="ul1">
+                    <li>
+                        <a href="#">
+                            <i class="uil uil-dialpad-alt"></i>
+                            555 555 555
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="uil uil-envelope-alt"></i>
+                            admin@famliyproducts.com
+                        </a>
+                    </li>
+                </ul>
+                <ul class="ul2">
+                    <li>
+                        <a href="#">
+                            <i class="uil uil-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="uil uil-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="uil uil-google"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="uil uil-linkedin-alt"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-row2">
+        <div class="container">
+            <div class="row">
+                <ul class="ul1">
+                    <li class="heading">
+                        <h4>التصنيفات</h4>
+                    </li>
+                    <ul class="ul-sub">
+                        <li>الفواكه والخضروات</li>
+                        <li>البقالة والمواد الغذائية الأساسية</li>
+                        <li>منتجات الألبان والبيض</li>
+                        <li>المشروبات</li>
+                        <li>الوجبات الخفيفة</li>
+                    </ul>
+                </ul>
+                <ul class="ul1">
+                    <li class="heading">
+                        <h4>روابط مفيدة</h4>
+                    </li>
+                    <ul class="ul-sub">
+                        <li>الفواكه والخضروات</li>
+                        <li>منتجات الألبان والبيض</li>
+                        <li>المشروبات</li>
+                        <li>الوجبات الخفيفة</li>
+                        <li>البقالة والمواد الغذائية الأساسية</li>
+                    </ul>
+                </ul>
+                <ul class="ul1">
+                    <li class="heading">
+                        <h4>أفضل المدن</h4>
+                    </li>
+                    <ul class="ul-sub">
+                        <li>الفواكه والخضروات</li>
+                        <li>البقالة والمواد الغذائية الأساسية</li>
+                        <li>منتجات الألبان والبيض</li>
+                        <li>المشروبات</li>
+                        <li>الوجبات الخفيفة</li>
+                    </ul>
+                </ul>
+                <ul class="ul1">
+             
+             
+             
+                    <li class="heading thd">
+                        <h4><b>طرق الدفع</b></h4>
+                    </li>
+                    <ul class="ul-sub t flex">
+                        <li class="dbnt">
+                            <a href="">
+                                <img src="assets/images/svg/pyicon-1.svg" alt="" />
+                            </a>
+                        </li>
+                        <li class="dbnt">
+                            <a href="">
+                                <img src="assets/images/svg/pyicon-2.svg" alt="" />
+                            </a>
+                        </li>
+                        <li class="dbnt">
+                            <a href="">
+                                <img src="assets/images/svg/pyicon-3.svg" alt="" />
+                            </a>
+                        </li>
+                        <li class="dbnt">
+                            <a href="">
+                                <img src="assets/images/svg/pyicon-4.svg" alt="" />
+                            </a>
+                        </li>
+                        <li class="dbnt">
+                            <a href="">
+                                <img src="assets/images/svg/pyicon-6.svg" alt="" />
+                            </a>
+                        </li>
+                    </ul>
+                    <li class="heading thd">
+                        <h4><b>رسالة إخبارية</b></h4>
+                    </li>
+                    <ul class="ul-sub t flex nl">
+                        <form action="#">
+                            <input type="text" name="" placeholder="ايميل" />
+                            <button><i class="uil uil-telegram-alt"></i></button>
+                        </form>
+                    </ul>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-3">
+        <span> Developed by famliyproducts group</span>
+    </div>
+</section>
+<script src="assets/js/script.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
